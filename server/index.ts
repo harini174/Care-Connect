7487222fdc7b2d3aff4303643b0cfec51966efbb
Harini <harini.c2023cce@sece.ac.in>
2025-08-12 10:32:57 +0530

    fix: fresh install to resolve tooltip dependency

--- server/index.ts
+++ server/index.ts
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || "5000", 10);
   const host = "127.0.0.1";
 
-  // Fixed: Removed reusePort, used normal listen syntax
+  
   server.listen(port, host, () => {
     log(`Server running at http://${host}:${port}`);
   });
